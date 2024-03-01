6af6529012c94764f3afcb190697335d70c4ebd9
jonasarce1 <jonasarceg@gmail.com>
2024-03-01 14:23:06 +0100

    con env

--- db/PostDB.ts
+++ db/PostDB.ts
@@ -1,6 +1,10 @@
 import mongoose, {InferSchemaType} from "npm:mongoose"
 
 
+if (mongoose.connection.readyState === 0) {
+    await mongoose.connect(Deno.env.get("MONGO_URL")!);
+}
+
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema ({
